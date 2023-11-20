6ec1851aee86d8fbc20c54290741096838dd0ba9
ateebamateen21 <bintemateen21@gmail.com>
2023-11-20 23:08:16 +0500

    Added section 4

---
+++ src/components/Section4/Advantage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Col, Container, Row } from 'reactstrap'
+
+const Advantage = () => {
+    const bgImage={
+        backgroundImage: "url(https://droidbot-official.web.app/static/media/advantageRectangle.f6259d8691e918105c57.png)"
+    }
+  return (
+    <>
+    <Container  fluid className="bg-secondary " style={bgImage}>
+        <Row className='p-3'>
+            <Col className='text-center'>
+            <img src="https://droidbot-official.web.app/static/media/advantageBowl.1bd10dbccebb9f8cbf3e.png" alt="benefits of droidbot" className="img-fluid mx-auto " />
+            </Col>
+        </Row>
+    </Container>
+    </>
+  )
+}
+
+export default Advantage
--- src/views/MainView.js
+++ src/views/MainView.js
@@ -1,12 +1,14 @@
 import  HeroSection from '../components/Section1/HeroSection';
 import Customise from '../components/Section2/Customise';
 import Support from '../components/Section3/Support';
+import Advantage from '../components/Section4/Advantage';
 const AdminView = () => {
 	return (
 		<>
 			<HeroSection />
 			<Customise />
 			<Support />
+			<Advantage/>
 		</>
 	);
 };
